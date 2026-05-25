7da282dc8a5458af167a9a0074b7d90f271bd40c
xhiew <hieunx1@vnpay.vn>
2026-05-25 16:07:13 +0700

    add resource

---
+++ KOIIController/Sources/KOIIController/Resources/ResourceDefinitons.swift
@@ -0,0 +1,9 @@
+//
+//  ResourceDefinitons.swift
+//  KOIIController
+//
+//  Created by xhiew on 25/5/26.
+//
+
+import Foundation
+import MCP
---
+++ KOIIController/Sources/KOIIController/Resources/ResourceHandlers.swift
@@ -0,0 +1,9 @@
+//
+//  ResourceHandlers.swift
+//  KOIIController
+//
+//  Created by xhiew on 25/5/26.
+//
+
+import Foundation
+import MCP
