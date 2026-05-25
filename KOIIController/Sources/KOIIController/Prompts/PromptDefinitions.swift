2aaabbfd8cd29ee7b332a3d1cd76bab829845bf1
xhiew <hieunx1@vnpay.vn>
2026-05-25 09:54:06 +0700

    add prompt

---
+++ KOIIController/Sources/KOIIController/Prompts/PromptDefinitions.swift
@@ -0,0 +1,9 @@
+//
+//  PromptDefinitions.swift
+//  KOIIController
+//
+//  Created by xhiew on 23/5/26.
+//
+
+import Foundation
+import MCP
---
+++ KOIIController/Sources/KOIIController/Prompts/PromptHandlers.swift
@@ -0,0 +1,9 @@
+//
+//  PromptHandlers.swift
+//  KOIIController
+//
+//  Created by xhiew on 23/5/26.
+//
+
+import Foundation
+import MCP
